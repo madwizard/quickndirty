3d4c31499e76142443e6bc0128ca26cae5527374
Damian Wojslaw <damian.wojslaw@enovation.ie>
2015-09-04 16:37:22 +0200

    Fixes by dosiek. Only needs to mention package once per CVE

--- query_rpm_for_cves.pl
+++ query_rpm_for_cves.pl
@@ -1,29 +1,28 @@
 #!/usr/bin/perl -w
 use strict;
 
+# Call:
+# ./query_rpm_for_cves.pl
+# Kudos to dosiek for help
+
 my %cves;
 my $changelog;
 my $command;
 my $i = 0;
 
-foreach my $line (<STDIN>) {
-    chomp($line);
-    if( $line =~ /.rpm$/) {
+foreach my $file (glob('*.rpm')) {
 
-    $command = "/bin/rpm -qp --changelog $line";
+    $command = "/bin/rpm -qp --changelog $file";
 
     open(my $fh, '-|', "$command") or die $!;
 
     while ($changelog = <$fh>) {
-
         chomp($changelog);
         if($changelog =~ s/^.*(CVE-(\d+)-(\d+)).*$/$1/i){
-        push(@{$cves{$changelog}}, $line);
-     
-    }
+            push(@{$cves{$changelog}}, $file);
+        }
     $i++;
     }
-}
 }
 
 foreach my $group (keys %cves){
